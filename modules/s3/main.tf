07bf0dde37408b3c3e03474d5185705c05c4b759
rsradulescu <rsradulescu@gmail.com>
2021-01-16 23:20:59 -0300

    S3 Event for lambda

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "event_lambda_s3_bucket" {
-  bucket        = "example-event-bucket"
+  bucket        = "${var.project}-${var.env}-example-event-bucket"
   force_destroy = true
 }
